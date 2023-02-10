928a7a39ddaf878bf40ae30b70aafce7623a5496
Juan Gonzalez <gonzalesjuandavid608@gmail.com>
2023-02-10 15:29:40 -0500

    es12, es13

---
+++ src/es12/01-replaceAll.js
@@ -0,0 +1,12 @@
+const string = "JavaScript es un maravilloso lenguaje de  programacion";
+
+const replacedString = string.replace("JavaScript", "TypeScript");
+
+console.log(replacedString);
+
+
+const texto = 'Javascript es el mejor lenguaje. Javascript puede vivir en el navegador y en el servidor.'
+
+texto.replace('Javascript', 'TypeScript');
+
+texto.replaceAll('Javascript', 'TypeScript');
